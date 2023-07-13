97682f11e0f3204167bc810b57480b6cca12772c
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2023-07-13 16:20:39 -0400

    Time: 150 ms (58.61%), Space: 17.2 MB (25.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,37 +3,33 @@ class Solution:
         if not s or not t:
             return ""
         
-        l, r = 0, 0
-
+        l, r = 0, 0 
         dict_t = Counter(t)
+        word_s = {}
 
         required = len(dict_t)
-
-        window_s = {}
-
         formed = 0
 
         ans = float("inf"), None, None
 
         while r < len(s):
             char = s[r]
-            window_s[char] = window_s.get(char, 0) + 1
+            word_s[char] = word_s.get(char, 0) + 1
 
-            if char in dict_t and window_s[char] == dict_t[char]:
+            if char in dict_t and dict_t[char] == word_s[char]:
                 formed += 1
             
             while l <= r and formed == required:
                 char = s[l]
 
                 if r - l + 1 < ans[0]:
-                    ans = (r - l + 1, l, r)
+                    ans = (r-l+1, l, r)
                 
-                window_s[char] -= 1
-                if char in dict_t and window_s[char] < dict_t[char]:
+                word_s[char] -= 1
+                if char in dict_t and word_s[char] < dict_t[char]:
                     formed -= 1
                 
                 l += 1
             r += 1
         
-        return "" if ans[0] == float('inf') else s[ans[1]: ans[2]+1]
-
+        return "" if ans[0] == float("inf") else s[ans[1]:ans[2]+1]
