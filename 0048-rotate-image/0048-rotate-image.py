85c033728d71903496b2ebd3a115f1bbc9138270
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2023-08-25 21:55:02 -0400

    Time: 31 ms (98.90%), Space: 16.5 MB (12.99%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,25 +1,28 @@
 class Solution:
-
-    def transpose(self, matrix):
+    def inverseRows(self, matrix):
+        m = len(matrix)
+        
+        for i in range(m//2):
+            for j in range(m):
+                matrix[i][j], matrix[m-i-1][j] = matrix[m-i-1][j], matrix[i][j]
+        return matrix
+    
+    def diagnoseMatrix(self, matrix):
+        
         n = len(matrix)
+        
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i+1):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
         return matrix
 
-    def reflect(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(n//2):
-                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
-        
-        return matrix
-
-
+            
+    
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        self.transpose(matrix)
-        self.reflect(matrix)
+        
+        matrix = self.inverseRows(matrix)
+        matrix = self.diagnoseMatrix(matrix)
+        return matrix
