d3e95ce9f0c028b1b11124110d69f6272af41e78
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2023-08-29 15:52:45 -0400

    Time: 73 ms (91.33%), Space: 26 MB (60.66%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -4,43 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
         if not head:
             return []
-
+        
         slow, fast = head, head
-
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
         
-
         dummy, head_1 = None, slow.next
-
         while head_1:
             temp = head_1.next
             head_1.next = dummy
             dummy = head_1
             head_1 = temp
-
-        slow.next = None
         
+        slow.next = None
         head_2, head_3 = head, dummy
-
         while head_3:
-            next_1 = head_2.next
-            next_2 = head_3.next
+            next_2 = head_2.next
+            next_3 = head_3.next
             
             head_2.next = head_3
-            head_3.next = next_1
-
-            head_2 = next_1
-            head_3 = next_2
+            head_3.next = next_2
             
+            head_2 = next_2
+            head_3 = next_3
+    
         return head
             
-
+            
