76e5921193bfe40a174f5233f639081b2ca730e6
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2023-08-30 23:48:44 -0400

    Time: 32 ms (93.11%), Space: 16.3 MB (65.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -9,7 +9,14 @@ class Solution:
         elif n == 2:
             return 2
         
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        dp = [0 for _ in range(n+1)]
+        
+        dp[1], dp[2] = 1, 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
         
         
         
