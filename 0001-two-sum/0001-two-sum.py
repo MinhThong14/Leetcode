bfa9a031b6a909c7a165f8b67eb741ec6d7264d2
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2023-08-31 00:02:48 -0400

    Time: 76 ms (54.87%), Space: 18 MB (5.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,7 +8,6 @@ class Solution:
         
         l, r = 0, len(nums)-1
         
-        print(nums)
         while l < r:
             if nums[l][0] + nums[r][0] == target:
                 return [nums[l][1], nums[r][1]]
