5ae78e69c665ac18f56e032b87e6dd9fc1fe3eee
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2023-09-03 16:48:29 -0400

    Time: 113 ms (74.70%), Space: 17.1 MB (59.41%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,33 +3,40 @@ class Solution:
         if not s or not t:
             return ""
         
-        l, r = 0, 0 
-        dict_t = Counter(t)
-        word_s = {}
-
-        required = len(dict_t)
+        t_dict = Counter(t)
+        word_chr = {}
+        
+        l, r = 0, 0
+        
+        ans = (float('inf'), None, None)
+        
         formed = 0
-
-        ans = float("inf"), None, None
-
+        
         while r < len(s):
-            char = s[r]
-            word_s[char] = word_s.get(char, 0) + 1
-
-            if char in dict_t and dict_t[char] == word_s[char]:
+            cur_char = s[r]
+            
+            if cur_char not in word_chr:
+                word_chr[cur_char] = 0
+            
+            word_chr[cur_char] += 1
+            
+            if cur_char in t_dict and word_chr[cur_char] == t_dict[cur_char]:
                 formed += 1
             
-            while l <= r and formed == required:
+            while l <= r and formed == len(t_dict):
+                
                 char = s[l]
-
+                
                 if r - l + 1 < ans[0]:
                     ans = (r-l+1, l, r)
                 
-                word_s[char] -= 1
-                if char in dict_t and word_s[char] < dict_t[char]:
-                    formed -= 1
+                word_chr[char] -= 1
+                if char in t_dict and word_chr[char] < t_dict[char]:
+                    formed -= 1   
                 
                 l += 1
+            
             r += 1
-        
-        return "" if ans[0] == float("inf") else s[ans[1]:ans[2]+1]
+        return "" if ans[0] == float('inf') else s[ans[1]:ans[2]+1]
+             
+            
