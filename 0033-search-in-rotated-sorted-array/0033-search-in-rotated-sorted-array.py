466897c2d37e99dd9ab21a7e19b0514b9edf57f4
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2023-09-16 10:53:08 -0400

    Time: 39 ms (95.36%), Space: 16.6 MB (68.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,38 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-#         l, r = 0, len(nums)-1
+        l, r = 0, len(nums)-1
         
-#         while l <= r:
-#             mid = l + (r-l)//2
-#             if nums[mid] == target:
-#                 return mid
-#             if nums[mid] < nums[r]:
-#                 if target > nums[mid] and target <= nums[r]:
-#                     l = mid + 1
-#                 else:
-#                     right = mid
-#             else:
-#                 if target < nums[mid] and target >= nums[l]:
-#                     r = mid
-#                 else:
-#                     l = mid + 1
-#         return -1
-        left, right = 0, len(nums)-1
-
-
-        while left <= right:
-            mid = left + (right - left)//2
+        while l <= r:
+            mid = l + (r-l)//2
             if nums[mid] == target:
                 return mid
-            if nums[mid] < nums[right]:
-                if target > nums[mid] and target <= nums[right]:
-                    left = mid + 1
+            if nums[mid] < nums[r]:
+                if target > nums[mid] and target <= nums[r]:
+                    l = mid + 1
                 else:
-                    right = mid
+                    r = mid
             else:
-                if target < nums[mid] and target >= nums[left]:
-                    right = mid
+                if target < nums[mid] and target >= nums[l]:
+                    r = mid
                 else:
-                    left = mid + 1
+                    l = mid + 1
         return -1
-                
