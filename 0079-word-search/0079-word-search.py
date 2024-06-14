5e81a9b91793c3cd5263b8020afaee2439c358a5
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-06-14 10:35:11 -0400

    Time: 3976 ms (56.75%), Space: 16.5 MB (58.70%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,6 +1,6 @@
 class Solution:
     
-    def dfs(self, i, j, board, word):
+    def dfs(self, board, word, i, j):
         if len(word) == 0:
             return True
         
@@ -10,20 +10,22 @@ class Solution:
         if board[i][j] == word[0]:
             board[i][j] = '#'
             
-            if self.dfs(i-1, j, board, word[1:]) or self.dfs(i+1, j, board, word[1:]) or self.dfs(i, j-1, board, word[1:]) or self.dfs(i, j+1, board, word[1:]):
+            if self.dfs(board, word[1:], i+1, j) or self.dfs(board, word[1:], i-1, j) or self.dfs(board, word[1:], i, j+1) or self.dfs(board, word[1:], i, j-1):
                 return True
             
             board[i][j] = word[0]
         
         return False
-    
+            
+        
+        
     
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
         
         for i in range(m):
             for j in range(n):
-                if self.dfs(i, j, board, word):
+                if self.dfs(board, word, i, j):
                     return True
         
         return False
