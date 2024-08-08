12254a8f94faeb7100f6ae8f7b7aa289983413bd
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-08-08 11:28:40 -0400

    Time: 72 ms (72.62%), Space: 16.7 MB (42.47%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,27 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
 
+
+class Solution:
     def isMatch(self, root, subRoot):
         if not root or not subRoot:
             return root == subRoot
         
         return root.val == subRoot.val and self.isMatch(root.left, subRoot.left) and self.isMatch(root.right, subRoot.right)
-
-
-
+    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
-        queue = [root]
-
+        queue = []
+        
+        queue.append(root)
+        
         while queue:
-            node = queue.pop(0)
-            if self.isMatch(node, subRoot):
+            cur_node = queue.pop(0)
+            
+            if self.isMatch(cur_node, subRoot):
                 return True
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-
+            
+            if cur_node.left:
+                queue.append(cur_node.left)
+            
+            if cur_node.right:
+                queue.append(cur_node.right)
+        
         return False
