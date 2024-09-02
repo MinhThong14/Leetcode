018937ca86f9cb82a89e41e93efd0c9627b44a37
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-09-02 11:39:07 -0400

    Time: 43 ms, Memory: 16.6 MB - LeetHub

---
+++ expressive-words/expressive-words.py
@@ -0,0 +1,46 @@
+class Solution:
+    
+    def validate_string(self, s, word):
+        count = 0
+        
+        m, n = len(s), len(word)
+        
+        if m < n:
+            return 0
+        
+        l, r = 0, 0
+        
+        i = 0
+        
+        while l < m and i < n:
+            if s[l] != word[i]:
+                return 0
+            
+            while r < m and s[l] == s[r]:
+                count += 1
+                r += 1
+
+            count_dup = 1
+            while i < n-1 and word[i] == word[i+1]:
+                count_dup += 1
+                i += 1
+            if (count < 3 and count_dup != count) or (count_dup > count):
+                return 0
+        
+            i += 1
+            l = r
+            count = 0
+            count_dup = 0
+            
+                          
+        return 1 if l == m and i == n else 0 
+    
+    def expressiveWords(self, s: str, words: List[str]) -> int:
+        
+        num_words = 0
+        
+        for word in words:
+            if self.validate_string(s, word) == 1:
+                num_words += 1
+        
+        return num_words
