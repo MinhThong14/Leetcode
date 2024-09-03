95444989c728d3a2e1250c6f4e2692b23a4ff0c6
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-09-03 16:28:13 -0400

    Time: 35 ms (52.02%), Space: 16.6 MB (5.31%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,41 @@
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        stack = []
+        
+        n = len(s)
+        
+        for i in range(n):
+            if (s[i] == ']'):
+                decodedString = []
+                
+                while stack[-1] != '[':
+                    decodedString.append(stack.pop())
+                
+                stack.pop()
+                base = 1
+                k = 0
+                
+                while len(stack) != 0 and stack[-1].isnumeric():
+                    k += int(stack.pop()) * base
+                    base *= 10
+                
+                
+                while k != 0:
+                    for j in range(len(decodedString)-1, -1, -1):
+                        stack.append(decodedString[j])
+                    k -= 1
+                
+            else:
+                stack.append(s[i])
+        print(stack)
+        
+        res = ""
+        
+        while stack:
+            res = stack.pop() + res
+        
+        return res
+                
+                    
