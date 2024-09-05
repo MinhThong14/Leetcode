1b8acda524840d7ebbf7eec36139b92b0b4c68eb
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-09-05 12:43:08 -0400

    Time: 30 ms (90.95%), Space: 16.6 MB (24.78%) - LeetHub

--- 0681-next-closest-time/0681-next-closest-time.py
+++ 0681-next-closest-time/0681-next-closest-time.py
@@ -1,15 +1,32 @@
 class Solution:
     def nextClosestTime(self, time: str) -> str:
-        cur = 60 * int(time[:2]) + int(time[3:])
-        allowed = {int(x) for x in time if x != ':'}
         
+        time_minutes = int(time[:2]) * 60 + int(time[3:])
+        
+        time_minutes += 1
+        
+        time_chars = set()
+        for char in time:
+            if char != ":":
+                time_chars.add(int(char))
+    
         while True:
-            cur = (cur+1) % (24*60)
-            is_allowed = True
-            for block in divmod(cur,60):
-                for digit in divmod(block, 10):
-                    if digit not in allowed:
-                        is_allowed = False
-                        break
-            if is_allowed:
-                return "{:02d}:{:02d}".format(*divmod(cur, 60))
+            if time_minutes == 1440:
+                time_minutes = 0
+            hours = time_minutes // 60
+            minutes = time_minutes % 60
+            
+            hour_first_digit = hours // 10
+            hour_second_digit = hours % 10 
+            
+            minutes_first_digit = minutes // 10
+            minutes_second_digit = minutes % 10
+            
+            if hour_first_digit in time_chars and hour_second_digit in time_chars and minutes_first_digit in time_chars and minutes_second_digit in time_chars:
+                return str(hour_first_digit) + str(hour_second_digit) + ":"+ str(minutes_first_digit) + str(minutes_second_digit)
+        
+            time_minutes += 1
+            
+            
+        
+        
