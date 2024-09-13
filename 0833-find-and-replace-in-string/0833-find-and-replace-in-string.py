b9884b5e214dc5fb743b11bc212e4a963630a4f4
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-09-13 18:33:29 -0400

    Time: 31 ms (97.53%), Space: 16.5 MB (97.14%) - LeetHub

---
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+        # iterate from the greater index to the smallest
+        for i, src, tg in sorted(list(zip(indices, sources, targets)), reverse=True):    
+            # if found the pattern matches with the source, replace with the target accordingly
+            if s[i:i+len(src)] == src: s = s[:i] + tg + s[i+len(src):]            
+        return s
+                
+                
