26b360f6416b3adbf0b31e60588f6a4f15e19f51
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-09-16 11:40:42 -0400

    Time: 111 ms (46.14%), Space: 17 MB (51.59%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
@@ -1,32 +1,37 @@
+"""
+1. [1, 0, 0, 0] -> num_one = 1 -> nums[0] = 1 -> n-1-0
+2. [0, 0, 0, 1] -> num_one = 1 -> nums[n-1] = 1 -> n-1-0
+3. [1, 0, 1, 0] -> num_one > 1 -> nums[0] = 1, nums[2] = 1 -> (2-0)/2 = 1
+"""
+
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
-        max_dist = 0
-        
-        first, second = -1, -1
         n = len(seats)
         
-        for i in range(n):
-            if seats[i] == 1:
-                if first == -1:
-                    first = i
-                    max_dist = max(max_dist, first)
-                else:
-                    second = i
-                    max_dist = max(max_dist, (second - first)//2)
-                    first = second
-    
+        l, r = 0, 0
+        max_dist = float('-inf')
+        res = 0
         
-        if second == -1:
-            mid = n//2
-            if first == mid:
-                return mid
-            elif first < mid:
-                return n-first-1
-            elif first > mid:
-                return first
+        while r < n:
+            if seats[r] == 1:
+                if l == 0 and seats[l] != 1 and r - l > max_dist:
+                    res = r - l
+                    max_dist = r - l
+                elif (r-l) // 2 > max_dist:
+                    res = (r-l) // 2
+                    max_dist = (r-l) // 2
+
+                l = r
+            r += 1
+        if r - l - 1 > max_dist:
+            res = r-l-1
         
-        max_dist = max(max_dist, n-second-1)
+        return res
         
-        return max_dist
-            
                     
+                        
+                
+                
+
+                    
+        
