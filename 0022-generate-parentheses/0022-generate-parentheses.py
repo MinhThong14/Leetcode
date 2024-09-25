48b6a1573f79633c4ce7f76dcc673f95878d0cb2
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-09-25 17:07:01 -0400

    Time: 45 ms (12.92%), Space: 16.9 MB (51.77%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,24 @@
 class Solution:
     def backtracking(self, cur_string, left, right, n, answer):
         if len(cur_string) == 2 * n:
-            answer.append("".join(cur_string))
+            answer.append(''.join(cur_string))
             return
+        
         if left < n:
-            cur_string.append("(")
-            self.backtracking(cur_string, left + 1, right, n, answer)
+            cur_string.append('(')
+            self.backtracking(cur_string, left+1, right, n, answer)
             cur_string.pop()
         if right < left:
-            cur_string.append(")")
-            self.backtracking(cur_string, left, right + 1, n, answer)
+            cur_string.append(')')
+            self.backtracking(cur_string, left, right+1, n, answer)
             cur_string.pop()
+        
     
     def generateParenthesis(self, n: int) -> List[str]:
         answer = []
         
         self.backtracking([], 0, 0, n, answer)
-            
+        
         return answer
         
         
