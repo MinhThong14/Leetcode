a3992c35cc30249ac84113e6cd6df4114a79571f
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-10-07 16:39:08 -0400

    Time: 69 ms, Memory: 18.9 MB - LeetHub

--- delete-and-earn/delete-and-earn.py
+++ delete-and-earn/delete-and-earn.py
@@ -3,19 +3,17 @@ class Solution:
         
         points = defaultdict(int)
         
-        max_number = 0
-        
+        max_num = 0
         for num in nums:
             points[num] += num
-            max_number = max(max_number, num)
-        
-        
-        max_points = [0] * (max_number+1)
+            max_num = max(max_num, num)
+            
+        max_points = [0] * (max_num + 1)
         max_points[0] = 0
         max_points[1] = points[1]
         
-        for num in range(2, max_number+1):
+        for num in range(2, max_num+1):
             max_points[num] = max(max_points[num-2] + points[num], max_points[num-1])
         
-        return max_points[max_number]
-        
+        return max_points[max_num]
+            
