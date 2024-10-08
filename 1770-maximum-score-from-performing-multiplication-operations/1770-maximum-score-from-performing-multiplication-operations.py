a12f923c88b7a5617dd1f16778da0e458bd6c064
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-10-08 11:29:25 -0400

    Time: 789 ms (70.50%), Space: 28.2 MB (67.54%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -1,14 +1,26 @@
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
-        @lru_cache(2000)
-        def dp(i, left):
-            if i == m:
-                return 0
-            
-            mult = multipliers[i]
-            right = n - 1 - (i - left)
-            
-            return max(mult * nums[left] + dp(i+1, left+1), mult * nums[right] + dp(i+1, left))
-        
         n, m = len(nums), len(multipliers)
-        return dp(0, 0)
+        dp = [[0 for _ in range(m+1)] for _ in range(m+1)]
+        
+        for i in range(m-1, -1, -1):
+            for left in range(i, -1, -1):
+                mult = multipliers[i]
+                right = n - 1 - (i-left)
+                dp[i][left] = max(mult * nums[left] + dp[i+1][left+1], mult * nums[right] + dp[i+1][left])
+        
+        return dp[0][0]
+        
+#         @lru_cache(2000)
+#         def dp(i, left):
+#             if i == m:
+#                 return 0
+            
+#             mult = multipliers[i]
+#             right = n - 1 - (i - left)
+            
+#             return max(mult * nums[left] + dp(i+1, left+1), mult * nums[right] + dp(i+1, left))
+        
+#         n, m = len(nums), len(multipliers)
+#         return dp(0, 0)
+        
