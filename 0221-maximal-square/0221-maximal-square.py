353ec3ea9d070d46a203f775d0e5cfede034536b
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-10-08 22:31:11 -0400

    Time: 753 ms (13.00%), Space: 19.7 MB (29.71%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -9,7 +9,7 @@ class Solution:
         for i in range(1, r + 1):
             for j in range(1, c + 1):
                 if matrix[i-1][j-1] == "1":
-                    dp[i][j] = (min(min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1)
+                    dp[i][j] = (min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1)
                     
                     max_len = max(max_len, dp[i][j])
         
