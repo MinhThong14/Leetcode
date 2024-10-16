b7eec5d531d60b9ce3bfe179282a28af457d8726
Tom Mai <64180472+MinhThong14@users.noreply.github.com>
2024-10-16 16:39:23 -0400

    Time: 103 ms (11.80%), Space: 18.3 MB (83.60%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from collections import deque
+
 
 """
 Ex1: [[1, 0]] -> 0-1
@@ -16,43 +15,45 @@ Ex2: [[1,0], [2, 0], [3, 1], [3, 2]] -> 0-1-2-3
 3. If len res == numCourses then return res, otherwise return empty array
 """
 
+from collections import defaultdict
+from collections import deque
+
 class Solution:
+    
     def topologicalSort(self, n, graph):
-        indegree = [0 for _ in range(n)]
+        indegree = [0] * n
         
         for values in graph.values():
             for value in values:
                 indegree[value] += 1
+        
+        visited = []
         
         queue = deque()
         
         for i in range(n):
             if indegree[i] == 0:
                 queue.append(i)
-        
-        visited = [0 for _ in range(n)]
-        
-        res = []
-        
+                visited.append(i)
+                
         while queue:
-            cur_node = queue.popleft()
-            res.append(cur_node)
-            visited[cur_node] = 1
-            
-            for neighboor in graph[cur_node]:
-                indegree[neighboor] -= 1
-                if not visited[neighboor] and indegree[neighboor] == 0:
-                    queue.append(neighboor)
+            cur_course = queue.popleft()
+            for course in graph[cur_course]:
+                indegree[course] -= 1
+                
+                if indegree[course] == 0 and course not in visited:
+                    queue.append(course)
+                    visited.append(course)
         
-        return res if len(res) == n else []
+        return visited if len(visited) == n else []
             
-            
+     
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
         
         for a, b in prerequisites:
             graph[b].append(a)
-            
+        
         return self.topologicalSort(numCourses, graph)
         
         
